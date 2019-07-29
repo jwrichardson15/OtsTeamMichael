483081016416695e273f09f79712fae9b533db77
Nicklaus McClendon <nicklaus@kulinacs.com>
2019-07-29 16:16:43 -0500

    Add Swagger documentation, test endpoint, and disable authentication

---
+++ src/main/java/com/credera/parks/common/controller/DefaultController.java
@@ -0,0 +1,27 @@
+package com.credera.parks.common.controller;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@RestController
+@RequestMapping("")
+public class DefaultController {
+
+    @GetMapping("/heartbeat")
+    @ApiOperation(value = "Heartbeat test", nickname = "getHeartbeat", notes = "Heartbeat check")
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "OK")
+    })
+    public ResponseEntity<String> heartbeat(HttpServletRequest request) {
+        return ResponseEntity.ok("healthy");
+    }
+}
---
+++ src/main/java/com/credera/parks/config/WebSecurityConfig.java
@@ -0,0 +1,21 @@
+package com.credera.parks.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+            http
+                .authorizeRequests().anyRequest().permitAll()
+                .and()
+                .csrf().disable()
+                .httpBasic().disable()
+                .formLogin().disable();
+    }
+}
