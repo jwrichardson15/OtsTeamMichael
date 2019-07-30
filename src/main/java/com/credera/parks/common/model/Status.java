e45fa455e536fca7f445a2104c8716a60cb79b98
Amanda Bsaibes <absaibes@gmail.com>
2019-07-30 11:54:55 -0500

    added the status model and repository

---
+++ src/main/java/com/credera/parks/common/model/Status.java
@@ -0,0 +1,20 @@
+package com.credera.parks.common.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "status")
+@Getter @Setter @NoArgsConstructor
+public class Status implements Serializable {
+    @Id
+    @GeneratedValue(generator = "id", strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+}
